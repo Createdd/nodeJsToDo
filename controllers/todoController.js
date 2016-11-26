15f4e75000f438c3e3746ba2e903f1a2af7ce747
Daniel Deutsch <deudan1010@gmail.com>
2016-11-26 13:38:05 +0100

    connecting app.get requests to mongoDB successfully

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -3,7 +3,7 @@
 var bodyParser=require("body-parser");
 var mongoose=require("mongoose");
 var urlencodedParser=bodyParser.urlencoded({extended:false});
-var data =[{item:"Javascript"}, {item:"node.js"}, {item:"react.js"}];
+//var data =[{item:"Javascript"}, {item:"node.js"}, {item:"react.js"}];
 
 mongoose.connect("mongodb://test:test@ds147537.mlab.com:47537/todo", ()=>{
   console.log("Connecting to MongoDB was successful!");
@@ -12,24 +12,24 @@ var todoSchema= new mongoose.Schema({
   item: String
 });//create a schema(a blueprint) for what the DB can expect with our data
 var Todo=mongoose.model("Todo", todoSchema);//allows to create new todos and push them to the DB
-var itemOne=Todo({item:"fighting"}).save((err)=>{
-  if(err) throw err;
-  console.log("item saved");
-});//create a testing item 
-
 
 module.exports=(app)=>{
   app.get("/", (req,res)=>{
-    res.render("todo", {todos: data});
+    Todo.find({},(err,data)=>{
+      if(err) throw err;
+      res.render("todo", {todos: data});
+    });//get data from mongodb and pass it to view
   });
   app.post("/",urlencodedParser,(req,res)=>{
-    data.push(req.body);
-    res.json(data);
+    var newTodo=Todo(req.body).save((err,data)=>{
+      if(err) throw err;
+      res.json(data);
+    });//get data from the view and add it to mongoDB
   });
   app.delete("/:item", (req,res)=>{
-    data=data.filter((todo)=>{
-      return todo.item.replace(/ /g, "-") !== req.params.item;//if true the item remains in the array
-    });
-    res.json(data);
+    Todo.find({item:req.params.item.replace(/\-/g, " ")}).remove((err,data)=>{
+      if(err) throw err;
+      res.json(data);
+    });//delete requested item from mongoDB
   });
 };
