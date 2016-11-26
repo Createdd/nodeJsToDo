04cec3cb33fe3f6b65500cea40a84a582a9ec87f
Daniel Deutsch <deudan1010@gmail.com>
2016-11-26 12:53:26 +0100

    add delete and add item functionality

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,13 +1,21 @@
 /* jshint node: true */
 /*jshint esversion: 6 */
+var bodyParser=require("body-parser");
+var urlencodedParser=bodyParser.urlencoded({extended:false});
+var data =[{item:"Javascript"}, {item:"node.js"}, {item:"react.js"}];
+
 module.exports=(app)=>{
   app.get("/", (req,res)=>{
-    res.render("todo");
+    res.render("todo", {todos: data});
   });
-  app.post("/", (req,res)=>{
-
+  app.post("/",urlencodedParser,(req,res)=>{
+    data.push(req.body);
+    res.json(data);
   });
-  app.delete("/", (req,res)=>{
-
+  app.delete("/:item", (req,res)=>{
+    data=data.filter((todo)=>{
+      return todo.item.replace(/ /g, "-") !== req.params.item;//if true the item remains in the array
+    });
+    res.json(data);
   });
 };
--- public/assets/todo-list.js
+++ public/assets/todo-list.js
@@ -2,33 +2,31 @@ $(document).ready(function(){
 
   $('form').on('submit', function(){
 
-      var item = $('form input');
-      var todo = {item: item.val()};
+      var item = $('form input');//target the input field
+      var todo = {item: item.val()};//store the value of the input in a variable as an object like in todoController
 
       $.ajax({
         type: 'POST',
-        url: '/todo',
+        url: '/',
         data: todo,
         success: function(data){
           //do something with the data via front-end framework
           location.reload();
-        }
-      });
-
+        }//get the data and reload page
+      });//AJAX: pass data from the variable todo as POST to the route
       return false;
-
-  });
+  });//set eventListener on submit
 
   $('li').on('click', function(){
-      var item = $(this).text().replace(/ /g, "-");
+      var item = $(this).text().replace(/ /g, "-");//store the text of the list item
       $.ajax({
         type: 'DELETE',
-        url: '/todo/' + item,
+        url: '/' + item,//send delete to this url
         success: function(data){
           //do something with the data via front-end framework
           location.reload();
-        }
-      });
+        }//get the data and reload page
+      });//AJAX
   });
 
 });
